a2bc48f0fc830627f660d7a0481fc40b4fe625e6
Gareth Brown <beedyg@googlemail.com>
2017-09-02 21:49:02 +0100

    Can_place?

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,19 @@
 class Board
+  attr_accessor :grid
+
+  def initialize
+    @grid = Array.new(3){Array.new(3){0}}
+  end
+
+  def place_x(row, col)
+    @grid[row][col] = 'X'
+  end
+
+  def can_place?(row, col)
+    answer = @grid[row][col] == nil
+  end
+
+  private
+    def place(position, marker)
+    end
 end
--- spec/lib/board_spec.rb
+++ spec/lib/board_spec.rb
@@ -6,4 +6,21 @@ describe Board do
     board = Board.new
     expect(board).not_to eq(nil)
   end
+
+  it "Has empty grid" do
+    board = Board.new
+    expect(board.grid).to eq([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+  end
+
+  it "Can place a piece" do
+    board = Board.new
+    board.place_x(1,1)
+    expect(board.grid[1][1]).to eq('X')
+  end
+
+  it "Test if can_place? works" do
+    board = Board.new
+    board.place_x(1,1)
+    expect(board.can_place?(1,1)).to eq(false)
+  end
 end
