76eb97ccc456b0d7249c8a6b33d4bfaa640689c8
Gareth Brown <beedyg@googlemail.com>
2017-09-03 15:38:03 +0100

    Refactor to use :nought and :cross

--- lib/board.rb
+++ lib/board.rb
@@ -1,70 +1,90 @@
 class Board
-  attr_accessor :grid
+  attr_accessor :grid, :status, :markers
 
-  def initialize
-    @grid = Array.new(3){Array.new(3){0}}
-  end
-
-  def place_x_at(row, col)
-    @grid[row][col] = -1 if can_place?(row, col)
-  end
-
-  def place_0_at(row, col)
-    @grid[row][col] = 1 if can_place?(row, col)
+  def initialize(dimension = 3)
+    @grid = Array.new(dimension){Array.new(dimension){0}}
+    @markers = { cross: 1, nought: -1}
+    @status = :play
   end
 
   def can_place?(row, col)
-    answer = @grid[row][col] == 0
+    @grid[row][col] == 0
   end
 
-  def winning_move?
-    answer = win_by_diag? || win_by_row? || win_by_col?
+  def place_at(row, col, marker)
+    #break if !valid_markers.all_keys.includes?(marker)
+    @grid[row][col] = markers[marker] if can_place?(row, col)
+    review_status
   end
 
-  def moves_left?
-    sum = 0
-    @grid.each do |row|
-      row.each { |v| sum += v.abs}
+  def review_status
+    if status == :play
+      #status = :in_play if @grid.flatten.count {|g| g != 0} > 0
+    #elsif
+
     end
-    sum == 9
   end
 
-  def win_by_row?
-    answer = false
+  def won_by_row
     @grid.each do |row|
-      sum = 0
-      row.each {|v| sum += v}
-      answer = true if sum.abs == 3
+      sum = row.inject(:+)
+      return sum <=> 0 if sum.abs == grid.size
     end
-    answer
-  end
+    0
+  end 
 
-  def win_by_col?
-    answer = false
-    sum_col = [0,0,0]
-    @grid.each do |row|
-      row.each_with_index {|v, col_i| sum_col[col_i] += v}
-    end
-    answer = true if sum_col.include?(-3) || sum_col.include?(3)
-  end
+  
 
-  def win_by_diag?
-    answer = win_by_opp_diag? || win_by_norm_diag?
-  end
 
-  def win_by_norm_diag?
-    answer = false
-    sum = 0
-    (0..2).each { |i| sum += @grid[i][i] }
-    answer = true if sum.abs == 3
-  end
 
-  def win_by_opp_diag?
-    answer = false
-    sum = 0
-    (0..2).each { |i| sum += @grid[i][(i-2).abs] }
-    answer = true if sum.abs == 3
-  end
+  # def winning_move?
+  #   answer = win_by_diag? || win_by_row? || win_by_col?
+  # end
+
+  # def moves_left?
+  #   sum = 0
+  #   @grid.each do |row|
+  #     row.each { |v| sum += v.abs}
+  #   end
+  #   sum == 9
+  # end
+
+  # def win_by_row?
+  #   answer = false
+  #   @grid.each do |row|
+  #     sum = 0
+  #     row.each {|v| sum += v}
+  #     answer = true if sum.abs == 3
+  #   end
+  #   answer
+  # end
+
+  # def win_by_col?
+  #   answer = false
+  #   sum_col = [0,0,0]
+  #   @grid.each do |row|
+  #     row.each_with_index {|v, col_i| sum_col[col_i] += v}
+  #   end
+  #   answer = true if sum_col.include?(-3) || sum_col.include?(3)
+  # end
+
+  # def win_by_diag?
+  #   answer = win_by_opp_diag? || win_by_norm_diag?
+  # end
+
+  # def win_by_norm_diag?
+  #   answer = false
+  #   sum = 0
+  #   (0..2).each { |i| sum += @grid[i][i] }
+  #   answer = true if sum.abs == 3
+  # end
+
+  # def win_by_opp_diag?
+  #   answer = false
+  #   sum = 0
+  #   (0..2).each { |i| sum += @grid[i][(i-2).abs] }
+  #   answer = true if sum.abs == 3
+  # end
 
 
 end
--- spec/lib/board_spec.rb
+++ spec/lib/board_spec.rb
@@ -15,52 +15,62 @@ describe Board do
   end
 
   it "Can place an Cross" do
-    @board.place_x_at(1,1)
-    expect(@board.grid[1][1]).to eq(-1)
-  end
-
-  it "Can place a Nought" do
-    @board.place_0_at(1,1)
+    @board.place_at(1, 1, :cross)
     expect(@board.grid[1][1]).to eq(1)
   end
 
+  it "Can place a Nought" do
+    @board.place_at(1, 1, :nought)
+    expect(@board.grid[1][1]).to eq(-1)
+  end
+
   it "Test if can_place? works" do
-    @board.place_x_at(1,1)
+    expect(@board.can_place?(1, 1)).to eq(true)
+    @board.place_at(1, 1, :cross)
     expect(@board.can_place?(1,1)).to eq(false)
-    expect(@board.can_place?(0,1)).to eq(true)
   end
 
-  it "Test if No more moves left" do
-    @board.grid.each_with_index do |row, row_i|
-      row.each { |col| @board.place_x_at(row_i,col) }
-    end
-    expect(@board.moves_left?).to eq(false)
+  it "Test if recognises row win" do
+    expect(@board.won_by_row).to eq(0)
+    @board.place_at(0, 0, :cross)
+    @board.place_at(0, 1, :cross)
+    @board.place_at(0, 2, :cross)
+    expect(@board.won_by_row).to eq(1)
   end
 
-  it "All row winning condition recognised" do
-    [[1,1,1],[1,1,0],[1,0,1]].each_with_index do |row, row_i|
-      row.each_with_index  { |col, col_i| @board.place_x_at(row_i,col_i) if col == 1 }
-    end
-    expect(@board.win_by_row?).to eq(true)
-    expect(@board.win_by_col?).to eq(true)
-    expect(@board.win_by_norm_diag?).to eq(true)
-    expect(@board.win_by_opp_diag?).to eq(true)
-  end
+  # it "Test if No more moves left" do
+  #   @board.grid.each_with_index do |row, row_i|
+  #     row.each { |col| @board.place_x_at(row_i,col) }
+  #   end
+  #   expect(@board.moves_left?).to eq(false)
+  # end
 
-  it "Can tell who won Xs" do
-    [[1,1,1],[1,1,0],[1,0,1]].each_with_index do |row, row_i|
-      row.each_with_index  { |col, col_i| @board.place_x_at(row_i,col_i) if col == 1 }
-    end
+  # it "All row winning condition recognised" do
+  #   [[1,1,1],[1,1,0],[1,0,1]].each_with_index do |row, row_i|
+  #     row.each_with_index  { |col, col_i| @board.place_x_at(row_i,col_i) if col == 1 }
+  #   end
+  #   expect(@board.win_by_row?).to eq(true)
+  #   expect(@board.win_by_col?).to eq(true)
+  #   expect(@board.win_by_norm_diag?).to eq(true)
+  #   expect(@board.win_by_opp_diag?).to eq(true)
+  # end
 
-    expect(@board.get_winner).to eq(1)
-  end
+  # it "Can tell who won Xs" do
+    
+  #   #Diagonal
+  #   @board.place_x_at(0,0)
+  #   @board.place_x_at(1,1)
+  #   @board.place_x_at(2,2)
 
-  it "Can tell who won 0s" do
-    [[-1,-1,-1],[-1,-1,0],[-1,0,-1]].each_with_index do |row, row_i|
-      row.each_with_index  { |col, col_i| @board.place_0_at(row_i,col_i) if col == -1 }
-    end
+  #   expect(@board.status).to eq(:won_by_x)
+  # end
 
-    expect(@board.get_winner).to eq(-1)
-  end
+  # it "Can tell who won 0s" do
+  #   [[-1,-1,-1],[-1,-1,0],[-1,0,-1]].each_with_index do |row, row_i|
+  #     row.each_with_index  { |col, col_i| @board.place_0_at(row_i,col_i) if col == -1 }
+  #   end
+
+  #   expect(@board.get_winner).to eq(-1)
+  # end
 
 end
