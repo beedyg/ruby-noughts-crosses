7b2b4318dfe378afbd837905e2b80911db3a69ab
Gareth Brown <g.p.brown@brighton.ac.uk>
2017-09-03 22:19:25 +0100

    Added play - this won't be tesed, moving to player next

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,20 @@
-require 'board'
-
 class Game
   attr_accessor :board
 
   def initialize
     @board = Board.new
+  end
+
+  def start
+    puts "Welcome to a Game of Noughts and Crosses"
+    num_human_players = -1
+    while ![0, 1, 2].include? num_human_players do
+      puts "How many human players (0, 1 or 2)?"
+      num_human_players = gets.chomp.to_i
+    end
+    puts "You chose #{num_human_players} human player#{'s' if num_human_players != 1}"
+
+    ## next steps
+
   end
 end
